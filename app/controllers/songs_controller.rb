5ad37719f07d63940b0a5f80d94541f3b0e758cf
J-Agens <johntagens@gmail.com>
2019-04-03 18:53:52 -0400

    more progress

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,29 +1,35 @@
+require 'rack-flash'
 class SongsController < ApplicationController
   set :views, Proc.new { File.join(root, "../views/songs") }
-
+  use Rack::Flash
   get '/songs' do
     @songs = Song.all
     erb :songs
   end
 
-  # get '/songs/new' do
-  #   @song = Song.new
-  #   erb :new_songs
-  # end
+  get '/songs/new' do
+    @song = Song.new
+    erb :new_songs
+  end
+
+  post '/songs' do
+    artist = Artist.find_or_create_by(name: params["artist"])
+    @song = Song.create(name: params[:name], artist_id: artist.id)
+    @song.genre_ids = params[:genres]
+    # params[:genres].each do |genre|
+    #   @song.genres << Genre.find(genre)
+    # end
+    # binding.pry
+    flash[:message] = "Successuflly created song."
+    redirect "/songs/#{@song.slug}"
+  end
 
   get '/songs/:slug' do
-    # binding.pry
     @song = Song.find_by_slug(params[:slug])
-
     erb :show_song
   end
 
-  # post '/songs' do
-  #   @song = Song.create(name: params["Name"])
-  #   @song.artist = Artist.find_by(name: params["artist"])
-  #   # @song.genre_id = Genre.find_by_slug(params[:genre]).id
-  #   redirect "/songs/#{@song.slug}"
-  # end
+
 
 
 
